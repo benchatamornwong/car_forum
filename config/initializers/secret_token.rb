ae3a92f5e18f855c8247da6e3813feb9e251de46
Wongsarun Chatamornwong <benchatamornwong@gmail.com>
2014-03-05 23:22:07 +0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarForum::Application.config.secret_key_base = '479f4057da21b0e3c8b638ad76fea14cb13667ea434df99ed5a5b31f5c40278574ed083b9f70aeb83693cd458f93bb8a17a7393219569f4428623c58f9d094a0'
